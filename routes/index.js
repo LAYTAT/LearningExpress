0c5116624fd1e22b67469bc96fa5af2caf8ae4f3
LAYTAT <591084988@qq.com>
2020-07-22 14:40:55 +0800

    express demo v0.0.1

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+router.use('/static', express.static('static'));
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/', function(req, res){
+  res.send('Got a POST request');
+});
+
+router.put('/user', function(req, res){
+  res.send('Got a PUT request at /user');
+});
+
+router.delete('/user', function(req, res) {
+  res.send('Got a DELETE request at /user');
+});
+
+module.exports = router;
